c988f02a4bc85b1ab66471f7777619c42b4dc419
moonlightlane <wangzichao6@gmail.com>
2015-03-26 21:07:04 -0500

    added guitar synthesizer

--- MPM_script.m
+++ MPM_script.m
@@ -1,9 +1,3 @@
-W = 2048; % window size
-[data, fs] = audioread('violin_test_g4.wav');
-f = zeros(1,ceil(length(data)/W));
-pointer = 1;
-while pointer < length(f)
-    f(pointer) = MPM_pitch_detection('violin_test_g4.wav',1+(pointer-1)*W, W);
-    pointer = pointer + 1;
-end
-%f(pointer) = MPM_pitch_detection('violin_test_g4.wav',1+(pointer-1)*W, W);
+close all
+
+[F,NOTE] = MPM('violin_test_g4.wav', 2048);
